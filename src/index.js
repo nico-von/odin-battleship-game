653aaf1088c1b94a47ebea87344c4bd8a58bc44f
m0rningstra <m0rningstra@pngu.localdomain>
2025-03-21 14:44:48 +0800

    Add css structure of header, main, footer

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import { body } from "./helpers/dompoints";
 import { footer } from "./view/navigation/footer";
 import { header } from "./view/navigation/header";
 import { main } from "./view/navigation/main";
+import "./styles/style.css"
 
 body.append(header, main, footer);
