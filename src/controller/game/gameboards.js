21552799d527efacf607ddd70176eb61b52b96fa
m0rningstra <m0rningstra@pngu.localdomain>
2025-04-07 11:45:11 +0800

    Put on a controller ships and boards of players

---
+++ src/controller/game/gameboards.js
@@ -0,0 +1,4 @@
+import { Gameboard } from "../../model/Gameboard";
+
+export const userGameboard = new Gameboard(10, 10);
+export const rivalGameboard = new Gameboard(10, 10);
--- src/view/navigation/main.js
+++ src/view/navigation/main.js
@@ -1,13 +1,16 @@
 import { createGameboardUIElement, placeShip } from "../gameboard/gameboard";
 
-const userGameboard = createGameboardUIElement(10, 10, true);
-const oppGameboard = createGameboardUIElement(10, 10, false);
+const userGameboardUI = createGameboardUIElement(10, 10, true);
+const oppGameboardUI = createGameboardUIElement(10, 10, false);
+
 const main = document.createElement("main");
 main.append(userGameboard, oppGameboard);
 
-placeShip(userGameboard, {length: 3}, "h", 5, 6);
-placeShip(userGameboard, {length: 1}, "v", 0, 3);
-placeShip(userGameboard, {length: 2}, "h", 3, 3);
-placeShip(userGameboard, {length: 3}, "v", 4, 5);
+// placeShip(userGameboard, {length: 3}, "h", 5, 6);
+// placeShip(userGameboard, {length: 1}, "v", 0, 3);
+// placeShip(userGameboard, {length: 2}, "h", 3, 3);
+// placeShip(userGameboard, {length: 3}, "v", 4, 5);
+
+
 
 export { main };
