ba57cfe0c6db36e9fab74e88480ce88fdfd00f35
nico-von <131730901+nico-von@users.noreply.github.com>
2025-05-14 15:50:39 +0800

    Make rival autoattack after user attack

--- src/controller/game/gameplay-events.js
+++ src/controller/game/gameplay-events.js
@@ -1,38 +1,79 @@
-const RECEIVE_ATTACK_EVENT = new CustomEvent("receiveattack", { bubbles: true, cancelable: false });
 
-let isUserFirstToAttack = false;
+let isUserTurn = false;
 let rGameboard = null;
 let uGameboard = null;
 let uGameboardUI = null;
 let rGameboardUI = null;
 
 export function startGameFunction(e, userGameboardUI, rivalGameboardUI, userGameboard, rivalGameboard, playButton) {
-    //clone userGameboardUi
+    //clone userGameboardUi to lock it
     const userGameboardUIClone = userGameboardUI.cloneNode(true);
     const userGameboardParentNode = userGameboardUI.parentNode;
     userGameboardParentNode.replaceChild(userGameboardUIClone, userGameboardUI);
+
     rivalGameboardUI.removeChild(playButton);
     rivalGameboardUI.classList.remove("awaiting-placement");
-    rivalGameboardUI.addEventListener("click", e => gameplayEventListener(e));
-    isUserFirstToAttack = getRandomNumber() === 0;
+
+    rivalGameboardUI.addEventListener("click", e => rivalHitListener(e));
+
+    isUserTurn = getRandomNumber() === 0;
     // this below centralises the gameplay, this will allow future player to player battles.
     uGameboard = userGameboard;
     rGameboard = rivalGameboard;
-    uGameboardUI = userGameboardUI;
+    uGameboardUI = userGameboardUIClone;
     rGameboardUI = rivalGameboardUI;
-    
     document.body.addEventListener("receiveattack", e => gamePlayManager(e));
+    attackUser();
     e.preventDefault();
 }
 
-function gamePlayManager(e) {
-    if (e.target === rGameboardUI) {
-        console.log(rGameboard);
+function attackUser() {
+    if (isUserTurn) {
+        return;
     }
+    console.log("user Hit");
+    isUserTurn = true;
+    uGameboardUI.dispatchEvent(new CustomEvent("receiveattack", {
+        bubbles: true,
+        cancelable: false,
+        detail: {
+            x: 0,
+            y: 0,
+            gameboard: uGameboard,
+        }
+    }));
 }
 
-function gameplayEventListener(e) {
-    e.currentTarget.dispatchEvent(RECEIVE_ATTACK_EVENT);
+function gamePlayManager(e) {
+    console.log(e.detail);
+    attackUser();
+}
+
+function rivalHitListener(e) {
+    if (!isUserTurn) {
+        return;
+    }
+
+    let { x, y } = e.target.dataset;
+
+    if (!x || !y) {
+        return;
+    }
+
+    x = Number(x);
+    y = Number(y);
+
+    console.log("rival Hit");
+    isUserTurn = false;
+    rGameboardUI.dispatchEvent(new CustomEvent("receiveattack", {
+        bubbles: true,
+        cancelable: false,
+        detail: {
+            x,
+            y,
+            gameboard: rGameboard,
+        }
+    }));
 }
 
 function getRandomNumber() {
